2bcd1973660e79f2826a5f7e82287b45f9c753ee
ccomyns <ccomyns27@gmail.com>
2026-01-30 17:25:28 +0000

    Deploying to gh-pages from @ ccomyns/ccomyns.github.io@a03ead24675a5a22cac5aabf4afdc9096aac574f 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "ccomyns/personal_page",
+    "data-repo": "ccomyns/ccomyns.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
